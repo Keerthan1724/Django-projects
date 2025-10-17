7a261e551955d3f1dfe8a7c890fa8507d8850565
Keerthan1724 <devadigakeerthan08@gmail.com>
2025-10-17 23:13:57 +0530

    Second update to project

--- main/views.py
+++ main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import ToDoList,Item
+from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>" % ls.name)
+    return render(response, 'main/list.html', {"ls":ls})
 
+def home(response):
+    return render(response, 'main/home.html')
+
+def create(response):
+    form = CreateNewList()
+    return render(response, 'main/create.html', {"form":form})
